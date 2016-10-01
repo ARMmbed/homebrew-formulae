14e531e91f2b2ba825f2df850940b3a42580dded
Ralph Martin <ralph@cs.cf.ac.uk>
2016-10-01 22:17:00 +0100

    Update arm-none-eabi-gcc.rb
    Move to q3-update, and switch to arm hosted site

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class ArmNoneEabiGcc < Formula
 
-  homepage 'https://launchpad.net/gcc-arm-embedded'
-  version '5.0-2016-q2-update'
+  homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
+  version '5.0-2016-q3-update'
 
-  url 'https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q2-update/+download/gcc-arm-none-eabi-5_4-2016q2-20160622-mac.tar.bz2'
-  sha256 'e175a0eb7ee312013d9078a5031a14bf14dff82c7e697549f04b22e6084264c8'
+  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/5_4-2016q3/gcc-arm-none-eabi-5_4-2016q3-20160926-mac,-d-,tar.bz2?product=GNU%20ARM%20Embedded%20Toolchain,64-bit,,Mac%20OS%20X,5-2016-q3-update'
+  sha256 '5656cdec40f99d5c054a85bbc694276e1c4a1488cdacbbc448bc6acd3bbe070d'
 
   def install
     system 'cp', '-r', 'arm-none-eabi', 'bin', 'lib', 'share', "#{prefix}/"
