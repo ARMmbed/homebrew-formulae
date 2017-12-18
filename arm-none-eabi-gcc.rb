3fdd6051b035a76d163770a70ce5c1b5435b511b
Niklas Hauser <niklas.hauser@arm.com>
2017-12-18 15:07:30 +0100

    Update ARM gcc to `7-2017-q4-major`

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -3,10 +3,10 @@ require 'formula'
 class ArmNoneEabiGcc < Formula
 
   homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
-  version '6-2017-q2-update'
+  version '7-2017-q4-major'
 
-  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-mac.tar.bz2'
-  sha256 '7d3080514a2899d05fc55466cdc477e2448b6a62f536ffca3dd846822ff52900'
+  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-mac.tar.bz2'
+  sha256 '89b776c7cf0591c810b5b60067e4dc113b5b71bc50084a536e71b894a97fdccb'
 
   def install
     (prefix/"gcc").install Dir["./*"]
