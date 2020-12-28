1a17be2f2b4f5dabeb5b8ab77b23a0cc39ec8002
Cesar Vandevelde <cesar.vandevelde@gmail.com>
2020-12-28 11:21:01 +0100

    Update to 10-2020-q4-major

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -3,10 +3,10 @@ require 'formula'
 class ArmNoneEabiGcc < Formula
 
   homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
-  version '9-2020-q2-update'
+  version '10-2020-q4-major'
 
-  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-mac.tar.bz2'
-  sha256 'bbb9b87e442b426eca3148fa74705c66b49a63f148902a0ea46f676ec24f9ac6'
+  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2'
+  sha256 'bed12de3565d4eb02e7b58be945376eaca79a8ae3ebb785ec7344e7e2db0bdc0'
 
   def install
     (prefix/"gcc").install Dir["./*"]
