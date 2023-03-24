23d1550cfbcfbcc2b752452dc50f7ec1679d613a
Mathias Brossard <mathias.brossard@arm.com>
2023-03-24 13:52:27 -0500

    Update to 10.3-2021.10

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -3,10 +3,10 @@ require 'formula'
 class ArmNoneEabiGcc < Formula
 
   homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
-  version '10.3-2021.07'
+  version '10.3-2021.10'
 
-  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.07/gcc-arm-none-eabi-10.3-2021.07-mac-10.14.6.tar.bz2'
-  sha256 '0a4554b248a1626496eeba56ad59d2bba4279cb485099f820bb887fe6a8b7ee4'
+  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2'
+  sha256 'fb613dacb25149f140f73fe9ff6c380bb43328e6bf813473986e9127e2bc283b'
 
   def install
     (prefix/"gcc").install Dir["./*"]
